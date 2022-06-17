062114129faa216d06b09fa8b2139318bfaacf2a
Thais Luza <thaisluza21@hotmail.com>
2022-06-17 10:07:42 -0300

    fix testes

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  rootDir: './__tests__/integration',
-  testSequencer: '../sequencer.js',
+  rootDir: './____/integration',
+  equencer: '../sequencer.js',
   setupFilesAfterEnv: ['../setup.js'],
   testRegex: './*\\.test\\.js$',
   testTimeout: 300000,
